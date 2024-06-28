7cc6824affb690408da7055520639bd224d7ab4a
Will Atkinson <willreubena@gmail.com>
2024-06-28 16:05:14 +0100

    form basic structure

---
+++ cash-tips/src/components/Form/index.jsx
@@ -0,0 +1,31 @@
+
+function Form() {
+    // Your app should display a form with the following fields:
+        // Total bill (the total price of the restaurant bill)
+        // Number of people (how many people are sharing the bill)
+        // Tip percentage (what % do you want to tip)
+        // Submit button
+        // When a user clicks the submit button, calculate the total bill plus tip and display it, and calculate how much each person needs to pay and display it.
+    
+    return (
+        <>
+            <form>
+                <label htmlFor="total" >Total Bill</label>
+                <input id="total" type="text" />
+
+                <label htmlFor="total" >Total Bill</label>
+                <input id="total" type="text" />
+                
+                <label htmlFor="total" >Total Bill</label>
+                <input id="total" type="text" />
+
+                <button type="submit" ></button>
+                
+
+            </form>
+
+        </>
+    )
+}
+
+export default Form
